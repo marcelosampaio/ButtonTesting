29b918c355c7a7f0380954c39978c973aa3300ef
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-06-10 08:39:54 -0300

    Initial Commit

---
+++ ButtonTesting/ButtonTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonTestingApp.swift
+//  ButtonTesting
+//
+//  Created by Marcelo Sampaio on 10/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ButtonTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
